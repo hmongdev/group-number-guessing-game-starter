d853045efcd52af2dc65832ee88c56cacffe96cf
Avery Yang <averyyang2674@gmail.com>
2022-06-23 14:20:36 -0500

    complete getGuesses function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,21 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+    console.log('jquery is loaded!');
+
+    $('#submit').on('click', getGuesses);
+}
+// //player's arrays to delete later
+// let guessListOne = [];
+// let guessListTwo = [];
+// let guessListThree = [];
+// let guessListFour = [];
+
+function getGuesses() {
+    //grab the guess
+    const currentGuess = {
+        currentGuess: Number($('#player-one').val()),
+    };
+    //check to see if currentGuess is showing
+    console.log(currentGuess.currentGuess);
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+//player's arrays
+let guessListOne = [];
+let guessListTwo = [];
+let guessListThree = [];
+let guessListFour = [];
+
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
@@ -12,6 +18,13 @@ app.use(express.static('server/public'));
 // GET & POST Routes go here
 
 
+
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+    console.log('Server is running on port', PORT);
+});
+
+// All random number stuff should happen on the server: Generation of the random number upon game start as well as comparisons between guesses and the random correct answer.
+
+// generate random number
+let randomNumber = Math.ceil(Math.random() * 25);
+console.log(randomNumber);
